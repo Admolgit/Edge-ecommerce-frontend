0f59b9dc703a676a0baf18067ed018bb2c91d6d8
Admolgit <zealous890@gmail.com>
2022-06-19 14:14:04 +0100

    another commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Signup from './controllers/User/signup';
+import Signin from './controllers/User/Signin';
+import Home from './controllers/Pages/Home';
+import Menu from './controllers/Pages/Menu';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Menu />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/signin" component={Signin} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
